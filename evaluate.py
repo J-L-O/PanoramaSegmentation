dfc10ebcda9a3fe23e0ab5ff05eb076eca90bea7
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2020-03-25 17:28:26 +0100

    Refactoring

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,5 @@
 import click as click
-from PIL import Image
-from keras_segmentation.data_utils.data_loader import get_pairs_from_paths
-from tqdm import tqdm
-import numpy as np
+from keras_segmentation.predict import evaluate_from_files
 
 
 @click.command()
@@ -11,33 +8,7 @@ import numpy as np
 @click.option('--n_classes', '-gt', default=16, help='The number of classes.', type=int)
 def evaluate(predictions, ground_truth, n_classes):
 
-    paths = get_pairs_from_paths(ground_truth, predictions)
-    paths = list(zip(*paths))
-    inp_images = list(paths[0])
-    annotations = list(paths[1])
-
-    tp = np.zeros(n_classes)
-    fp = np.zeros(n_classes)
-    fn = np.zeros(n_classes)
-    n_pixels = np.zeros(n_classes)
-
-    for inp, ann in tqdm(zip(inp_images, annotations)):
-        pr = np.array(Image.open(inp))
-        gt = np.array(Image.open(ann))
-
-        pr = pr.flatten()
-        gt = gt.flatten()
-
-        for cl_i in range(n_classes):
-            tp[cl_i] += np.sum((pr == cl_i) * (gt == cl_i))
-            fp[cl_i] += np.sum((pr == cl_i) * (gt != cl_i))
-            fn[cl_i] += np.sum((pr != cl_i) * (gt == cl_i))
-            n_pixels[cl_i] += np.sum(gt == cl_i)
-
-    cl_wise_score = tp / (tp + fp + fn + 0.000000000001)
-    n_pixels_norm = n_pixels / np.sum(n_pixels)
-    frequency_weighted_IU = np.sum(cl_wise_score * n_pixels_norm)
-    mean_IU = np.mean(cl_wise_score)
+    frequency_weighted_IU, mean_IU, cl_wise_score = evaluate_from_files(predictions, ground_truth, n_classes)
 
     print(f'frequency_weighted_IU: {frequency_weighted_IU}, mean_IU: {mean_IU}, class_wise_IU: {cl_wise_score}')
 
